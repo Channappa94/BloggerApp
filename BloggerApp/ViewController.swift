16da4bab0e15468c366396bdafb681d231bdcabc
Phani_Kiran <pret321@gmail.com>
2019-08-06 18:28:10 -0500

    Initial Commit

---
+++ BloggerApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BloggerApp
+//
+//  Created by IMCS2 on 8/6/19.
+//  Copyright © 2019 com.phani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITabBarDelegate, UITableViewDataSource {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+
+
+}
+
